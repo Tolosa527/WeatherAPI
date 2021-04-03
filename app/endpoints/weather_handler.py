40ad54323720dfc654c7bcc24bb4c83e2a6977d4
matias-zulberti <matias.zulberti@globant.com>
2021-04-03 12:39:12 -0300

    Working in progress

--- app/endpoints/weather_handler.py
+++ app/endpoints/weather_handler.py
@@ -1,6 +1,7 @@
 import redis
 import json
-from fastapi import APIRouter, Depends
+from redis import exceptions as redis_exceptions
+from fastapi import APIRouter, Depends, HTTPException
 from app.models.weather import Location,WeatherResponse
 from .services.weather import Weather
 from .services.cache_handler import get_redis_client
@@ -15,19 +16,26 @@ async def Wheater_by_city_state(
         client: redis = Depends(get_redis_client)
     ):
     result = ''
-    if client.get(f'{location.city}'):
-        cache_data = json.loads(client.get(location.city))
-        print(f'DATA FROM CACHE:\n{cache_data}')
-        return cache_data
     try:
-        weahter_instance = Weather(
-            city=location.city,
-            country=location.country
-        )
-        result = weahter_instance.get_from_open_weather()
-        result_json = json.dumps(result)
-        print(f'THIS DATA WAS SAVED IN CACHE:\n{result_json}')
-        client.set(f'{location.city}', result_json,ex=EXPIRATION_TIME)
-    except Exception as e:
-        print(e)
+        if client.get(f'{location.city}'):
+            cache_data = json.loads(client.get(location.city))
+            print(f'DATA FROM CACHE:\n{cache_data}')
+            return cache_data
+    except redis_exceptions.ConnectionError as redis_error:
+        print(f'[WARNING] - ERROR TRYING TO GET INFO FROM CACHE: {redis_error}')
+        try:
+            weahter_instance = Weather(
+                city=location.city,
+                country=location.country
+            )
+            result = weahter_instance.get_from_open_weather()
+            result_json = json.dumps(result)
+            if not redis_error:
+                client.set(f'{location.city}', result_json,ex=EXPIRATION_TIME)
+                print(f'THIS DATA WAS SAVED IN CACHE:\n{result_json}')
+        except Exception as e:
+            raise HTTPException(
+                status_code=500,
+                detail='Error'
+            )
     return result
