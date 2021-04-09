bfe16c7e1b9d874ce39624391383cb9989011060
matias-zulberti <matias.zulberti@globant.com>
2021-04-09 17:27:29 -0300

    minimal change

--- app/endpoints/services/cache_handler.py
+++ app/endpoints/services/cache_handler.py
@@ -2,6 +2,5 @@ import redis
 import settings
 
 def get_redis_client():
-    #settings.REDIS_HOST
-    client = redis.Redis(host='6379', port=settings.REDIS_PORT)
+    client = redis.Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT)
     return client
